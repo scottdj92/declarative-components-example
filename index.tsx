d22a68d375539aa3024b28d9d8e07129e3062997
Scott Jones <scottdj92@gmail.com>
2018-04-15 00:21:06 -0400

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import { render } from "react-dom";
+import App from "./src/app";
+
+render(<App/>, document.getElementById("main"));
---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App: React.SFC = () => (
+    <h1>hello world</h1>
+);
+
+export default App;
